a068776e8d87bef21d609e08d3787eab2b926585
krish-98 <muralikrishnanm98@gmail.com>
2023-06-28 22:18:39 +0530

    created POST method and getting data from query params, headers and body

--- index.js
+++ index.js
@@ -7,6 +7,29 @@ app.get("/", (req, res) => {
   res.send("Hello HTTP Server!")
 })
 
+// Making POST Req
+app.post("/handleSum", (req, res) => {
+  // Getting query params
+  // const counter = req.query.counter
+  // const sumValue = calculateSum(counter)
+
+  // Getting headers data
+  console.log(req.headers)
+  const counter = req.headers.counter
+  const sumValue = calculateSum(counter)
+
+  res.send(`The sum of 1 to ${!counter ? 1 : counter} is ${sumValue}`)
+})
+
 app.listen(port, () => {
   console.log(`The server is running on port ${port}`)
 })
+
+/**
+ * To send inputs to the back-end we can use,
+ * 1. query params => To acces the query params in the desired router handler (It means when a certain route is hit). We use req.query."keyname"
+ * 2. headers => We can't directly send headers via browser URL. It mostly accepts GET req. So we're using POSTMAN to use send POSt req. To acces the headers we use req.headers."keyname"
+ * 3. body => Express doesn't give body out of the box. We have to install a library called body-parser and use in the middleware
+ *
+ *
+ */
