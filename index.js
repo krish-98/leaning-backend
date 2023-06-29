292a72ea9a30dc4dcc78ceff1578a6a2c1192242
krish-98 <muralikrishnanm98@gmail.com>
2023-06-29 21:33:32 +0530

    server sends 3 things status code, body, headers

--- index.js
+++ index.js
@@ -24,17 +24,44 @@ app.use(middleware1)
 
 // Making GET req
 app.get("/", (req, res) => {
-  res.send("Hello HTTP Server!")
+  // Sends plain text
+  // res.send("Hello HTTP Server!")
+
+  // Sends HTML
+  res.send(`<!DOCTYPE html>
+  <html lang="en">
+    <head>
+      <meta charset="UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <title>Document</title>
+    </head>
+    <body>
+      <h1>Bonjour!</h1>
+    </body>
+  </html>
+  `)
+
+  // Sends JSON
+  // res.send({ name: "Murali Krishnan" })
 })
 
-const calculateSum = (num = 1) => {
+const calculateSum = (num) => {
   let sum = 0
 
-  for (let i = 0; i <= num; i++) {
+  for (let i = 1; i <= num; i++) {
     sum += i
   }
 
   return sum
+}
+
+const calculateMul = (num) => {
+  let multiple = 1
+
+  for (let i = 1; i <= num; i++) {
+    multiple *= i
+  }
+  return multiple
 }
 
 // Making POST Req
@@ -50,9 +77,21 @@ app.post("/handleSum", (req, res) => {
 
   // console.log(req.body)
   const counter = req.body.counter
-  const sumValue = calculateSum(counter)
 
-  res.send(`The sum of 1 to ${!counter ? 1 : counter} is ${sumValue}`)
+  const sumValue = calculateSum(counter)
+  const multipleValue = calculateMul(counter)
+
+  const answerObject = {
+    sum: sumValue,
+    multiple: multipleValue,
+  }
+
+  res.send(answerObject)
+})
+
+// We can change the status code
+app.get("/howdy", (req, res) => {
+  res.status(401).send("Yeah, I'm good man")
 })
 
 app.listen(port, () => {
@@ -63,7 +102,16 @@ app.listen(port, () => {
  * To send inputs to the back-end we can use,
  * 1. query params => To acces the query params in the desired router handler (It means when a certain route is hit). We use req.query."keyname"
  * 2. headers => We can't directly send headers via browser URL. It mostly accepts GET req. So we're using POSTMAN to use send POSt req. To acces the headers we use req.headers."keyname"
- * 3. body => Express doesn't give body out of the box. We have to install a library called body-parser and use in the middleware
+ * 3. body => Express doesn't give body out of the box. We have to install a library called body-parser and use it in the middleware
  *
+ *
+ */
+
+/**
+ * What does Server send?
+ * Server sends 3 things
+ * 1. Status code
+ * 2. Body (In the body it can send Plain text, JSON or HTML File)
+ * 3.
  *
  */
---
+++ otherRoutes.js
@@ -0,0 +1,18 @@
+// app.get("/handleSum", (req, res) => {
+//   const counter = req.query.counter
+//   const sumValue = calculateSum(counter)
+
+//   res.send(`The sum of 1 to ${!counter ? 1 : counter} is ${sumValue}`)
+// })
+
+// app.get("/muralikrishnan", (req, res) => {
+//   res.send("He's 25 years old")
+// })
+
+// app.get("/after/*", (req, res) => {
+//   res.send("Yooo")
+// })
+
+// app.get("*", (req, res) => {
+//   res.send("uh!")
+// })
