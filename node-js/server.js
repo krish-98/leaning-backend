bd8f06ac4e31e9e49d4c6f69767f932099e1a895
krish-98 <muralikrishnanm98@gmail.com>
2023-07-31 12:55:52 +0530

    route handlers & chainging route handlers

--- node-js/server.js
+++ node-js/server.js
@@ -5,6 +5,7 @@ const path = require("path")
 const PORT = process.env.PORT || 3000
 
 app.get("^/$|/index(.html)?", (req, res) => {
+  // serving files
   //   res.sendFile("./views/index.html", {
   //     root: __dirname,
   //   })
@@ -20,9 +21,38 @@ app.get("/old-page(.html)?", (req, res) => {
   res.redirect(301, "/new-page.html")
 })
 
+// route handlers
+app.get(
+  "/hello(.html)?",
+  (req, res, next) => {
+    console.log("attempted to load hello.html")
+    next()
+  },
+  (req, res) => {
+    res.send("Hello World")
+  }
+)
+
+// chaining route handlers
+const one = (req, res, next) => {
+  console.log("one")
+  next()
+}
+
+const two = (req, res, next) => {
+  console.log("two")
+  next()
+}
+const three = (req, res, next) => {
+  console.log("three")
+  res.send("Finished!")
+}
+
+app.get("/chain(.html)?", [one, two, three])
+
 // 404 page
-app.get("*", (req, res) => {
-  res.status(404).sendFile(__dirname, "views", "404.html")
-})
+// app.get("/*", (req, res) => {
+//   res.status(404).sendFile(__dirname, "views", "404.html")
+// })
 
 app.listen(PORT, () => console.log(`Server running on ${PORT}`))
