b1ac2675697a6801941014222e88041140fa329c
krish-98 <muralikrishnanm98@gmail.com>
2023-08-01 09:03:37 +0530

    routing

--- node-js/server.js
+++ node-js/server.js
@@ -41,53 +41,42 @@ app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 
 // serve static files
-app.use(express.static(path.join(__dirname, "public")))
+app.use("/", express.static(path.join(__dirname, "public")))
+app.use("/subdir", express.static(path.join(__dirname, "public")))
 
-app.get("^/$|/index(.html)?", (req, res) => {
-  // serving files
-  //   res.sendFile("./views/index.html", {
-  //     root: __dirname,
-  //   })
-  res.sendFile(path.join(__dirname, "views", "index.html"))
-})
-
-app.get("/new-page(.html)?", (req, res) => {
-  res.sendFile(path.join(__dirname, "views", "new-page.html"))
-})
-
-// redirects
-app.get("/old-page(.html)?", (req, res) => {
-  res.redirect(301, "/new-page.html")
-})
+// routes
+app.use("/", require("./routes/root"))
+app.use("/subdir", require("./routes/subdir"))
+app.use("/employees", require("./routes/api/employees"))
 
 // route handlers
-app.get(
-  "/hello(.html)?",
-  (req, res, next) => {
-    console.log("attempted to load hello.html")
-    next()
-  },
-  (req, res) => {
-    res.send("Hello World")
-  }
-)
+// app.get(
+//   "/hello(.html)?",
+//   (req, res, next) => {
+//     console.log("attempted to load hello.html")
+//     next()
+//   },
+//   (req, res) => {
+//     res.send("Hello World")
+//   }
+// )
 
 // chaining route handlers
-const one = (req, res, next) => {
-  console.log("one")
-  next()
-}
+// const one = (req, res, next) => {
+//   console.log("one")
+//   next()
+// }
 
-const two = (req, res, next) => {
-  console.log("two")
-  next()
-}
-const three = (req, res, next) => {
-  console.log("three")
-  res.send("Finished!")
-}
+// const two = (req, res, next) => {
+//   console.log("two")
+//   next()
+// }
+// const three = (req, res, next) => {
+//   console.log("three")
+//   res.send("Finished!")
+// }
 
-app.get("/chain(.html)?", [one, two, three])
+// app.get("/chain(.html)?", [one, two, three])
 
 // 404 page
 // app.get("/*", (req, res) => {
